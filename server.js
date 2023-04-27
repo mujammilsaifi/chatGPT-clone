aafed0dd44755cce3a0881e7139c97fe5c131aa9
Mujammil <116416462+mujammilsaifi@users.noreply.github.com>
2023-04-27 22:14:51 +0530

    serverjs file changed

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const connectDB = require("./config/Db");
 const authRoute=require("./routes/authRoutes");
 const openaiRoute=require("./routes/openaiRoutes")
 const errorHandler = require("./middleware/errorMiddleware");
+const path =require("path");
 
 //DOTENV
 dotenv.config();
@@ -24,6 +25,7 @@ app.use(BodyParser.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cors());
 app.use(errorHandler);
+app.use(express.static(path.join(__dirname,'./client/build')));
 
 //API ROUTES 
 app.use("/api/v1/auth",authRoute);
